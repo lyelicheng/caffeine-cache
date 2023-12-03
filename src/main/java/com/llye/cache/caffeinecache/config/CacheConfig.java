5206374a2fc8e80345af1f977ca72f4d6f39c485
joycelye <joycelye1991@gmail.com>
2023-12-03 23:23:16 +0800

    code refactoring

--- src/main/java/com/llye/cache/caffeinecache/config/CacheConfig.java
+++ src/main/java/com/llye/cache/caffeinecache/config/CacheConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 
+import java.time.Duration;
+
 @Configuration
 @EnableCaching
 public class CacheConfig {
@@ -14,7 +16,14 @@ public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager("customersCache");
-        cacheManager.setCaffeine(Caffeine.newBuilder().recordStats());
+        cacheManager.setCaffeine(caffeineCacheBuilder());
         return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                       .initialCapacity(100)
+                       .maximumSize(500)
+                       .expireAfterWrite(Duration.ofMinutes(10));
     }
 }
