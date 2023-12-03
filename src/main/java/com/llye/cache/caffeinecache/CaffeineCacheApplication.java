f8b6abf6ca51e611da6dac4c6e8110ef3e97990b
joycelye <joycelye1991@gmail.com>
2023-12-03 22:14:47 +0800

    init

---
+++ src/main/java/com/llye/cache/caffeinecache/CaffeineCacheApplication.java
@@ -0,0 +1,13 @@
+package com.llye.cache.caffeinecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeineCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffeineCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llye/cache/caffeinecache/CaffeineCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llye.cache.caffeinecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeineCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
